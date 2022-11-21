b490a447eedd3d64709dc0e49abb4a95b8a5afde
Shong Lee <Shonglee17@gmail.com>
2022-11-21 11:43:47 -0600

    working on showCollection question

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -10,46 +10,51 @@ function addToCollection(...x){
 
 
 const music1 = {
-    title1 : 'Hot Fuss',
-    artist1 : 'The Killers',
-    yearPublished1 : 2004,
-}//end music
+    title : 'Homesick',
+    artist : 'A Day To Remember',
+    yearPublished : 2009,
+}
 
 const music2 = {
-    title2 : 'Sams Town',
-    artist2 : 'The Killers',
-    yearPublished2 : 2006, 
-}//end music1
+    title : 'Sams Town',
+    artist : 'The Killers',
+    yearPublished : 2006, 
+}
 
 const music3 ={
-    title3 : 'Dusk And Summer',
-    artist3 : 'Dashboard Confessionals',
-    yearPublished3 : 2006,
+    title : 'Luxury Disease',
+    artist : 'One Ok Rock',
+    yearPublished: 2022,
 }
 const music4 = {
-    title4 : 'Secret Valentine',
-    artist4 : 'We The Kings',
-    yearPublished4 : 2007,
+    title : 'Secret Valentine',
+    artist : 'We The Kings',
+    yearPublished : 2007,
 }
 const music5 ={
-    title5 : 'Homesick',
-    artist5 : 'A Day To Remember',
-    yearPublished5 : 2009,
+    title : 'Hot Fuss',
+    artist : 'The Killers',
+    yearPublished : 2004,
 }
 const music6 = {
-    title6 : 'Luxury Disease',
-    artist6 : 'One Ok Rock',
-    yearPublished6: 2022,
+    title : 'Dusk And Summer',
+    artist : 'Dashboard Confessionals',
+    yearPublished : 2006,
 }
 
-console.log('add this object to addToCollection:', addToCollection(music1) )
-console.log('add this object to addToCollection:', addToCollection(music2) )
-console.log('add this object to addToCollection:', addToCollection(music3) )
-console.log('add this object to addToCollection:', addToCollection(music4) )
-console.log('add this object to addToCollection:', addToCollection(music5) )
-console.log('add this object to addToCollection:', addToCollection(music6) )
+console.log('add in:', addToCollection(music1, music2, music3, music4, music5, music6) )
+
 console.log(collection)
 
-function showCollection (){
+
+
+function showCollection (x){
+    for (i=0 ; i<x.length ; i++){
     
-}
+        console.log()
+    }//end for
+    return x.length;
+}//end showCollection
+console.log('this is how many items are in the array', showCollection(collection) );
+
+
