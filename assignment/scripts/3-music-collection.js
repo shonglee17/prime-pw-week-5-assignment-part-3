3d9191ff7cfdde2f1b41417fdf204f234b2538c7
Shong Lee <Shonglee17@gmail.com>
2022-11-23 14:31:19 -0600

    revised addToCollection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,48 +1,62 @@
 console.log('***** Music Collection *****')
 
-const music1 = {
-    title : 'Homesick',
-    artist : 'A Day To Remember',
-    yearPublished : 2009,
+
+// const music1 = {
+//     title : 'Homesick',
+//     artist : 'A Day To Remember',
+//     yearPublished : 2009,
     
-}
+// }
 
-const music2 = {
-    title : 'Sams Town',
-    artist : 'The Killers',
-    yearPublished : 2006, 
-}
+// const music2 = {
+//     title : 'Sams Town',
+//     artist : 'The Killers',
+//     yearPublished : 2006, 
+//     tracks : {'When You Were Young' : '3:40', 
+//             'Sams Town' : '4:06'}
+// }
 
-const music3 ={
-    title : 'Luxury Disease',
-    artist : 'One Ok Rock',
-    yearPublished: 2022,
-}
-const music4 = {
-    title : 'Secret Valentine',
-    artist : 'We The Kings',
-    yearPublished : 2007,
-}
-const music5 ={
-    title : 'Hot Fuss',
-    artist : 'The Killers',
-    yearPublished : 2004,
-}
-const music6 = {
-    title : 'Dusk And Summer',
-    artist : 'Dashboard Confessionals',
-    yearPublished : 2006,
-}
+// const music3 ={
+//     title : 'Luxury Disease',
+//     artist : 'One Ok Rock',
+//     yearPublished: 2022,
+//     tracks : ['Save Yourself : 3:18', 'Let Me Let You Go : 3:01', 'Neon : 3:05']
+// }
+// const music4 = {
+//     title : 'Secret Valentine',
+//     artist : 'We The Kings',
+//     yearPublished : 2007,
+//     tracks : ['Check Yes Juliet : 3:40', 'Stay Young : 2:48']
+// }
+// const music5 ={
+//     title : 'Hot Fuss',
+//     artist : 'The Killers',
+//     yearPublished : 2004,
+//     tracks : ['Mr Brightside : 3:45', 'All These Things Ive Done : 5:02']
+// }
+// const music6 = {
+//     title : 'Dusk And Summer',
+//     artist : 'Dashboard Confessionals',
+//     yearPublished : 2006,
+//     tracks : ['Stolen : 3:53', 'Currents : 4:24', 'So Long, So Long : 4:16']
+// }
+
+
 
 const collection = [];
 
-function addToCollection(...x){
-    console.log('in addToCollection')
-    collection.push(...x)
-    return collection;
+function addToCollection(title, artist, yearPublished){
+    let x = {title, artist, yearPublished}
+    collection.push(x)
+    return x;
 }//end addToCollection
 
-console.log('add in:', addToCollection(music1, music2, music3, music4, music5, music6) )
+addToCollection('Dusk And Summer','Dashboard Confessionals',2006)
+addToCollection('Homesick', 'A Day To Remember', 2009)
+addToCollection('Sams Town', 'The Killers', 2006)
+addToCollection('Luxury Disease', 'One Ok Rock',2022)
+addToCollection('Secret Valentine','We The Kings', 2007,)
+addToCollection('Hot Fuss','The Killers', 2004)
 console.log(collection)
 
 
@@ -51,6 +65,7 @@ function showCollection (collection){
     
     for (i=0 ; i<collection.length ; i++){
         console.log(collection[i].title + " by " + collection[i].artist + " published in " + collection[i].yearPublished)
+        // console.log(forEach(collection[i].tracks))
     }//end for
     return collection.length;
 }//end showCollection
@@ -73,15 +88,17 @@ console.log(Artist)
 //Stretch Goals
 let newCollection = []
 
-function search(a , b){
+function search(a , b, c){
     for (i=0; i<collection.length; i++){
-        if (a === collection[i].artist && b === collection[i].yearPublished ){
+        if (a === collection[i].artist && b === collection[i].yearPublished && c === collection[i].tracks){
             newCollection.push(collection[i])
         }//end if 
-        else if ( a === '' || b ===''){
+        else if ( a === ' ' || b === ' ' || c === ' '){
                 return collection
         }//end else if
     }//end for
     return newCollection
     }//end search
-console.log('check to see if artist and title is in array:', search( 'The Killers' , 2004  ) )
+console.log('check to see if artist and title is in array:', search( ) )
+
+
